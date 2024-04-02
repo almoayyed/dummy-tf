f07ae5c3eb2f7afee62075dcdba68b012e9740de
sandeepk <awsyadav@gmail.com>
2024-04-02 12:42:27 +0300

    updated

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,6 @@
 resource "aws_instance" "example_server" {
   ami           = "ami-0843a4d6dc2130849"
   instance_type = "t2.micro"
-  subnet_id     = "subnet-03088a642eaa82c88"
 
   tags = {
     Name = "createdBySandeep-Testing"
